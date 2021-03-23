52d795d675cd18e33d87432986a7eba3959d7094
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:40:50 -0300

    P08: Script 03 exercise A

---
+++ scrip 03/a/index.js
@@ -0,0 +1,16 @@
+let a = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+console.log(`${a[4]} and ${a[10]}`);
