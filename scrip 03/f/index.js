54cb660f9cad26e042077aab86a1c32dd4da15bc
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:54:22 -0300

    P08: Script 03 exercise F

---
+++ scrip 03/f/index.js
@@ -0,0 +1,16 @@
+let a = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+let b = a.join("-");
+console.log(b);
