86005f4bddba52cd150461f85be67a692d9aca44
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 14:08:46 -0300

    P08: Script 04 exercise B

---
+++ scrip 04/b/index.js
@@ -0,0 +1,8 @@
+let age = 23;
+if (age < 2) alert("Bebe");
+else if (age >= 2 && age <= 12) alert("Niño");
+else if (age >= 13 && age <= 19) alert("Adolecente");
+else if (age >= 20 && age <= 30) alert("Joven");
+else if (age >= 31 && age <= 60) alert("Adulto");
+else if (age >= 61 && age <= 75) alert("Adulto mayor");
+else alert("Anciano");
