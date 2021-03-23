9b4d51f6199fa8c4367e58169a4cf8d899833943
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:35:40 -0300

    P08: Script 02 exercise F

---
+++ scrip 02/f/index.js
@@ -0,0 +1,9 @@
+let a = "throughout pronunciation";
+let b =
+  a.substring(0, 1).toUpperCase() +
+  a.substring(2, 10).toLowerCase() +
+  " " +
+  a.substring(11, 12).toUpperCase() +
+  a.substring(12, 24).toLowerCase();
+  
+console.log(b);
