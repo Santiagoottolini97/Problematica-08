50fbade3729df6161b143526f0a212c94689ca92
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:45:28 -0300

    P08: Script 03 exercise C

---
+++ scrip 03/c/index.js
@@ -0,0 +1,17 @@
+let a = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+a.unshift("INICIO");
+a.push("FIN");
+console.log(a);
