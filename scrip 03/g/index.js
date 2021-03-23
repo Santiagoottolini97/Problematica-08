39c6b9ca387cc348300b02efdd98429bc43057e8
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:57:06 -0300

    P08: Script 03 exercise G

---
+++ scrip 03/g/index.js
@@ -0,0 +1,17 @@
+let a = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  let b = a.slice(4,11);
+  console.log(b);
+  
