a2d2082607dfd44e419c7ee673f457ff49f1ff03
santiago <santiago.ottolini97@hotmail.com>
2021-03-23 13:41:37 -0300

    P08: Script 03 exercise B

---
+++ scrip 03/b/index.js
@@ -0,0 +1,16 @@
+let a = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  
+  console.log(a.sort());
